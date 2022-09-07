34e85944ee9791632d8115a3f04659318593888e
changeqingteng <790792491@qq.com>
2022-09-07 10:47:14 +0800

    提交

--- src/entry.ts
+++ src/entry.ts
@@ -2,8 +2,8 @@ import { App } from "vue";
 import MyButton from "./Sbutton";
 import SFCButton from "./SFCbutton.vue";
 import JSXButton from "./JSXbutton";
-// import SButton from "./Button/Button.vue";  /// 引入引入SButtton的SFC文件类型 需配置vite.config.js
-import SButton from "./Button/index"; /// 引入SButtton的tsx文件类型 需配置vite.config.js
+import SButton from "./Button/Button.vue";  /// 引入引入SButtton的SFC文件类型 需配置vite.config.js
+// import SButton from "./Button/index"; /// 引入SButtton的tsx文件类型 需配置vite.config.js
 
 // 导出单独组件
 export { MyButton, SFCButton, JSXButton, SButton };
@@ -15,6 +15,6 @@ export default {
     app.component(MyButton.name, MyButton);
     app.component(SFCButton.name, SFCButton);
     app.component(JSXButton.name, JSXButton);
-    app.component('SButton', SButton);
+    app.component(SButton.name, SButton);
   },
 };
